46bcf4e689be8bd42bdc190c71b17d74f82d1a57
DESKTOP-2JVOCDB\timulys <timulys@gmail.com>
2018-07-07 21:11:09 +0900

    회원 가입, 회원정보 수정, 목록 등 기능 추가/수정

---
+++ src/main/java/net/slipp/domain/User.java
@@ -0,0 +1,80 @@
+package net.slipp.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(nullable = false, length = 20)
+	private String userId;
+	private String password;
+	private String name;
+	private String email;
+
+	public User() {
+	}
+
+	public User(String userId, String password, String name, String email) {
+		this.userId = userId;
+		this.password = password;
+		this.name = name;
+		this.email = email;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+	public void update(User updateUser) {
+		this.password = updateUser.getPassword();
+		this.name = updateUser.getName();
+		this.email = updateUser.getEmail();
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
+	}
+}
--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,27 +1,53 @@
 package net.slipp.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.slipp.domain.User;
+import net.slipp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-	private List<User> users = new ArrayList<User>();
+	//
+	@Autowired
+	private UserRepository userRepository;
 	
-	@PostMapping("/create")
-	public String create(User user) {
-		System.out.println("user : " + user);
-		users.add(user);
-		return "redirect:/list";
+	@GetMapping("/form")
+	public String from() {
+		return "/user/form";
 	}
 	
-	@GetMapping("/list")
+	@PostMapping("")
+	public String create(User user) {
+		userRepository.save(user);
+		return "redirect:/users";
+	}
+	
+	@GetMapping("")
 	public String list(Model model) {
-		model.addAttribute("users", users);
-		return "list";
+		model.addAttribute("users", userRepository.findAll());
+		return "/user/list";
+	}
+
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		User user = userRepository.findById(id).get();
+		model.addAttribute("user", user);
+		return "/user/updateForm";
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id, User updateUser) {
+		User user = userRepository.findById(id).get();
+		user.update(updateUser);
+		userRepository.save(user);
+		return "redirect:/users";
 	}
 }
