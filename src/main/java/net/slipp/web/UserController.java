e619b5ceb421311d6e398a99106947e0fbaabb7a
DESKTOP-2JVOCDB\timulys <timulys@gmail.com>
2018-07-10 00:13:38 +0900

    로그인 기능 구현

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,5 +1,7 @@
 package net.slipp.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,6 +20,30 @@ public class UserController {
 	//
 	@Autowired
 	private UserRepository userRepository;
+	
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "/user/login";
+	}
+	
+	@PostMapping("/login")
+	public String login(String userId, String password, HttpSession session) {
+		User user = userRepository.findByUserId(userId);
+		
+		if (user == null) {
+			System.out.println("Login Failure");
+			return "redirect:/users/loginForm";
+		}
+		
+		if (!password.equals(user.getPassword())) {
+			System.out.println("Login Failure");
+			return "redirect:/users/loginForm";
+		}
+		
+		session.setAttribute("user", user);
+		
+		return "redirect:/";
+	}
 	
 	@GetMapping("/form")
 	public String from() {
